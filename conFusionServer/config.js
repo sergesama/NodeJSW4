72c20e5fa5202a13ad7b07ab9eac090125c48e38
Sergey Gordon <sergesamas@gmail.com>
2021-03-16 17:57:32 +0300

    assigment 4

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '217675409748781',
+        clientSecret: '7bc6227f3ca4192da5b6c7e95ae1ba32'
+    }
 }
